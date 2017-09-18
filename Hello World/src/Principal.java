848d15ee82a88c94ce45160520145cdb9139b46b
Orlewilson B. Maia <orlewilson.maia@gmail.com>
2017-09-18 19:44:23 -0400

    Exemplo visto no laboratório no dia 22/08/2017

---
+++ Hello World/src/Principal.java
@@ -0,0 +1,44 @@
+/*
+ *	Disciplina: Programação Orientada a Objetos 
+ *  Professor: 	Orlewilson B. Maia
+ *  Turma:		Análise e Desenvolvimento de Sistemas, Gestão da TI 
+ *  			e Segurança da Informação
+ *  Data:		22/08/17
+ *  Autor:		Orlewilson B. Maia
+ *  Descrição:	Executar o projeto
+ * 	
+ */
+
+public class Principal {
+
+	// Método para executar o projeto
+	public static void main(String[] args) {
+
+		// imprime algo no console e pula uma
+		// linha
+		System.out.println("Olá Mundo Java!!!");
+		
+		// imprime algo no console e não pula 
+		// uma linha
+		System.out.print("Olá Mundo Java!!!");
+		System.out.print("Olá Mundo, TI!!!");
+		
+		// imprime um valor numérico
+		System.out.println(1234);
+		
+		// imprime um valor concatenado
+		System.out.println("teste " + "oi " + 123);
+		
+		// faz a soma e depois imprime o 
+		// resultado
+		System.out.println(1 + 2 + 3);
+		
+		// outras formas de operações de 
+		// concatenação
+		System.out.println("1" + 2 + 3);
+		
+		System.out.println(1 + 2 + "3");
+		
+		System.out.println("1" + (2 + 3));
+	}	
+}
